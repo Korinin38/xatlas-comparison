9e5baf6573666b74db7120767757b23aafbb9bae
Korinin38 <artkash@bk.ru>
2024-05-29 18:42:28 +0300

    refactor

--- results_aggregator.py
+++ results_aggregator.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 
 
@@ -11,19 +12,35 @@ def load_csv(filename: str):
 
 
 def filter_data(df: pd.DataFrame):
-	return df[df["Resolution"] == 4096]
+	# return df[(df["Resolution"] == 8192) & ((df["Old?"] == 1) & (df["BruteForce?"] == 1) | (df["Old?"] == 0) & (df["BruteForce?"] == 0))]
+	# return df[(df["Resolution"] == 8192) & (df["BruteForce?"] == 1)]
+	return df[(df["Resolution"] == 8192)]
 
 
 def generate_plot(df: pd.DataFrame, output_name: str):
 	new = df[df["Old?"] == 0]
 	old = df[df["Old?"] == 1]
-	plt.plot(old.index, old["Time"], label="old")
-	plt.plot(new.index, new["Time"], label="new")
-	plt.xlabel("Number of charts")
-	plt.ylabel("Time, s")
-	plt.legend()
-	plt.title("Comparison between new and old aglorithms")
-	plt.savefig(output_name)
+
+	x = np.arange(len(old.index))
+
+	plt.clf()
+	plt.figure(figsize=(20, 5))
+	plt.grid(which='major', axis='y')
+
+	old_better_time = np.where((old["Time"] < new["Time"]), old["Time"], 0)
+	plt.bar(x, old["Time"], label="old", color="tab:orange")
+	plt.bar(x, new["Time"], label="new", color="tab:blue")
+	plt.bar(x, old_better_time, color="tab:orange")
+	plt.ylabel("Время, с")
+	plt.ylim(1, 100000)
+	plt.yscale('log')
+
+	plt.xticks([])
+	plt.legend(loc="upper left")
+	plt.xlabel("Сортировка по возрастанию количества чартов")
+	plt.title("Сравнение старого и нового алгоритмов")
+	plt.savefig(output_name + '.png')
+
 
 
 if __name__ == "__main__":
