df6805385edef6294315177bae132234fc5bfe14
Korinin38 <artkash@bk.ru>
2024-05-29 18:54:56 +0300

    fix

--- results_aggregator.py
+++ results_aggregator.py
@@ -42,9 +42,33 @@ def generate_plot(df: pd.DataFrame, output_name: str):
 	plt.savefig(output_name + '.png')
 
 
+def generate_plot_PE(df: pd.DataFrame, output_name: str):
+	new = df[df["Old?"] == 0]
+	old = df[df["Old?"] == 1]
+
+	x = np.arange(len(old.index))
+
+	plt.clf()
+	plt.figure(figsize=(20, 5))
+	plt.grid(which='major', axis='y')
+
+	old_worse_PE = np.where((old["PE"] < new["PE"]), old["PE"], 0)
+	plt.bar(x, old["PE"], label="old", color="tab:orange")
+	plt.bar(x, new["PE"], label="new", color="tab:blue")
+	plt.bar(x, old_worse_PE, color="tab:orange")
+	plt.ylim(0, 100)
+	plt.ylabel("Плотность упаковки, %")
+
+	plt.xticks([])
+	plt.legend(loc="upper left")
+	plt.xlabel("Сортировка по возрастанию количества чартов")
+	plt.title("Сравнение старого и нового алгоритмов")
+	plt.savefig(output_name + '_PE.png')
+
 
 if __name__ == "__main__":
 	a = load_csv(sys.argv[1])
 	a = filter_data(a)
-	generate_plot(a, sys.argv[2])
+	generate_plot_time(a, sys.argv[2])
+	generate_plot_PE(a, sys.argv[2])
 
